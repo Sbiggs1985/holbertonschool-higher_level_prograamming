f3bddbe72f820813f4e02997b53a359fb49abccb
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-05-10 15:38:15 -0700

    Updates

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_last_digit(number):
+	lastDigit = 0
+	if number < 0:
+		number = abs(number)
+
+	lastDigit = number % 10
+
+	print(lastDigit, end='')
+	return(lastDigit)
