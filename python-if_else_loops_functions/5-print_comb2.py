c87f36aa548985c8be103d77da04d6eab3c66eba
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-05-10 20:51:39 -0700

    Updates

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-	if i == 99:
-		print("{}".format(i))
-	else:
-		print("{0:0=2d}, ".format(i), end='')
+    if(i < 99):
+        print("{0:0=2d}, ".format(i), end='')
+    else:
+        print("{00}".format(i))
