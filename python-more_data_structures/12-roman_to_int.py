64db19a047eb423d6f10de70646e47d4ad92617e
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-05-14 19:23:12 -0700

    Updates

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 100}
-    total = 0
-    if type(roman_string) is not str or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is none:
         return 0
-    if len(roman_string) == 1:
-        return romans.get(roman_string)
-    for i in range(len(roman_string)):
-        if i + 1 == len(roman_string):
-            total += romans.get(roman_string[i])
-        elif romans.get(roman_string[i]) >= romans.get(roman_string[i + 1]):
-            total += romans.get(roman_string[i])
+
+    roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman_values.get(char, 0)
+
+        if value >= prev_value:
+            total += value
         else:
-            total -= romans.get(roman_string[i])
-        return total
+            total -= value
+        prev_value = value
+    return total
