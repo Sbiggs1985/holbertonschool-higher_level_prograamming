50b8d495709f081dcf67173989c1340d942729ee
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-05-16 09:39:19 -0700

    Updates

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list:
-        new_list = my_list.copy()
-        for i in range(0, len(my_list)):
-            if my_list[i] % 2 == 0:
-                new_list[i] = True
-            else:
-                new_list[i] = False
-            return new_list
+    newList = []
+    len_list = len(my_list)
+    for index in range(len_list):
+        if my_list[index] % 2 == 0:
+            newList.append(True)
+        else:
+            newList.append(False)
+        return newList
