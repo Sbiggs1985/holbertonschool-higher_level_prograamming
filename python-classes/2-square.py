5c0d861bb9066c77ba2291fde66e0434183f299a
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-05-18 19:45:31 -0700

    Updates

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-Square = __import__('2-square').Square
+"""
+square
+"""
 
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+class Square:
+    """this class defines a square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
+    def area(self, size):
+        self.area = size * size
+    pass
