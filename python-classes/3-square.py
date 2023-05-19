45c61b6c3a0eeb4b917078a6743f711406d4ab14
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-05-18 19:54:35 -0700

    Updates

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""class Square that defines a square"""
+"""
+square
+"""
 
 
 class Square:
-    """class Square that defines a square"""
-    __size = None
-
+    """this class defines a square"""
     def __init__(self, size=0):
-        """Instation with optional size"""
-        if type(size) is int:
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError("Size must be >= 0")
-            else:
-                raise TypeError("size must be an integer")
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-        def area(self):
-            """Public instance method area"""
-            return self.__size * self.__size
+    def area(self):
+        return(self.__size * self.__size)
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
