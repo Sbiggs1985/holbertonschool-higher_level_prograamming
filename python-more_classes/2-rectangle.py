98d49843c896634f5f3639dc56e559b0751f608e
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-05-21 18:13:24 -0700

    Updates

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -5,35 +5,20 @@ Module for task 2. Area and Perimeter
 
 
 class Rectangle:
-
     def __init__(self, width=0, height=0):
-
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """
-        Returns the width of the Rectangle
-        """
-
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-         Checks the parameters and set the size of the Rectangle
-
-        Args:
-            value (int): The width of the Rectangle.
-
-        Raises:
-            TypeError: If `value` type is not `int`.
-            ValueError: If `value` is less than `0`.
-
-        """
-
-        self.__check_valid_width(value)
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -42,44 +27,14 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-
-        self.__check_valid_height(value)
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
 
-
-def __check_valid_width(self, width):
-
-    if self.__check_int_value(width) is False:
-        raise TypeError('width must be an integer')
-    if self.__check_positive_value(width) is False:
-        raise ValueError('width must be >= 0')
-
-
-def __check_valid_height(self, height):
-
-    if self.__check_int_value(height) is False:
-        raise TypeError('height must be an integer')
-
-    if self.__check_positive_value(height) is False:
-        raise ValueError('height must be >= 0')
-
-    def __check_int_value(self, value):
-        if type(value) is int:
-            return True
-
-        return False
-
-    def __check_positive_value(self, value):
-        if value >= 0:
-            return True
-
-        return False
-
     def area(self):
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
-            return 0
-
-        return self.__width * 2 + self.__height * 2
+        return 2 * (self.width + self.height)
