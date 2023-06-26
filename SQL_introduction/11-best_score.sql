63c8262c5011b0c88c7b1646297fc6d650cb6e39
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-06-26 13:50:48 -0700

    Updates

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- This is a comment
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- This is not a comment
+SELECT AVG(score) AS average FROM second_table;
