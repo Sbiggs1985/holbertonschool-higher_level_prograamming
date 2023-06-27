2e4f46d8c7b1413f99f7b70758f7b7d27bbb433d
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-06-27 16:47:55 -0700

    Updates

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- This is a comment
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@localhost' IDENTIFIED BY 'user_0d_1_pwd' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
