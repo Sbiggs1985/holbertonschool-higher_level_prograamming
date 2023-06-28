d90eb732068befb5526d0613c65336c10d79314a
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-06-28 12:29:44 -0700

    Updates

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
--- This is a comment
-CREATE DATABASE IF NOT EXISTS  hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- This is creating privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' identified by 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
