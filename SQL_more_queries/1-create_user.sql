29e59c11aa91fe33aea469e0aee81327463bfc0e
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-06-28 12:14:12 -0700

    Updates

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
--- This is not a comment
+-- Write a script to create MySQL server privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
