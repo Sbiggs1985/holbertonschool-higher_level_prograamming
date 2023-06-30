7e2adc3777ea3b06be11cb1721853949fc5cdee3
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-06-29 17:29:35 -0700

    Updates

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,4 @@
--- Create the database if it does not exist
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Create the user if it does not exist
+-- Write a script that creates the database
+CREATE DATABASE IF NOT EXISTS  hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privileges on the database to the user
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
