1ff35cde7b3deae8a31686c5bccfa82008799b9e
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-06-29 17:32:53 -0700

    Update

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,17 +1,9 @@
--- Creates the database named hbtn_0d_usa
--- IF NOT EXISTS checks for database
+-- Write a script that creates the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Creates the table within the database
--- USE hbtn_0d_usa - sets the currently active databsse to hbtn_0d_usa.
--- id INT - Represents an integer value
--- AUTO_INCREMENT - Automatically generates a unique vale for each new row inserted into the table
--- state_id INT NOT NULL - This line defines the state_id column of the cities table.
--- The INT cannot be NULL.
--- FOREIGN KEY - establishes a foreign key constraint on the state_id column.
-USE hbtn_0d_usa; CREATE TABLE IF NOT EXISTS cities (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES state(id)
-	);
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+);
