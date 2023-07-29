8465d5a3743bac62e681616714c426468aa98e23
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-07-29 10:06:02 -0700

    update

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let line1 = 'C is fun';
+const line1 = 'C is fun';
 console.log(line1);
-let line2 = 'Python is cool';
+const line2 = 'Python is cool';
 console.log(line2)
-let line3 = 'JavaScript is amazing';
+const line3 = 'JavaScript is amazing';
 console.log(line3)
