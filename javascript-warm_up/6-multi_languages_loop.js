bc62240eac02de665f05dde3899e656cd72f8da1
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-07-31 09:48:20 -0700

    update

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-const lines = [
-  'C is fun',
-  'Python is cool',
-  'JavaScript is amazing'
-];
-
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myVar.length; i++) {
+  console.log(myVar[i]);
 }
-
