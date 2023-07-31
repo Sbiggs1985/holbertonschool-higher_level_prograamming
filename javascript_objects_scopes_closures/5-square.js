12bd6a4693a2d7b5b837e7bcc28dc21d6ca48262
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-07-31 14:23:19 -0500

    update

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-}
-
-module.exports = Square;
+const Rectangle = require("./4-rectangle");
+module.exports = class Square extends Rectangle {
+    constructor(size) {
+        super(size, size);
+    }
+};
