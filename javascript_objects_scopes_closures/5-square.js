5503c333035dbf8d0afc5be489db78118c57093b
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-07-31 12:13:39 -0700

    update

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,41 +1,8 @@
-class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      // Create an empty object if w or h is not positive
-      return {};
-    }
-  }
-
-  print() {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        console.log("X".repeat(this.width));
-      }
-    }
-  }
-
-  rotate() {
-    // Exchange width and height
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double() {
-    // Multiply width and height by 2
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-module.exports = Rectangle;
-
+#!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor(size) {
-    // Call the constructor of the base class (Rectangle)
+  constructor (size) {
     super(size, size);
   }
 }
